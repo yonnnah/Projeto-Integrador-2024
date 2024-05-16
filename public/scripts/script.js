5ea081e040320118a0f96751df303faf00cd94e9
Jonas Miguel <jonasmiguel0077@gmail.com>
2024-05-16 01:37:37 -0300

    full application

---
+++ public/scripts/script.js
@@ -0,0 +1,19 @@
+
+// Adicionando hífen automaticamente ao digitar 3 números no input
+
+// <script>
+//             function formatarNumero(input) {
+//             let numero = input.value;
+//             numero = numero.replace(/\D/g, '');
+//             if (numero.length > 3) {
+//                 numero = numero.substring(0, 3) + '-' + numero.substring(3);
+//             }
+//             if (numero.length > 7) {
+//                 numero = numero.substring(0, 7) + '-' + numero.substring(7);
+//             }
+//             if (numero.length > 11) {
+//                 numero = numero.substring(0, 11) + '-' + numero.substring(11);
+//             }
+//             input.value = numero;
+// }
+// </script>
